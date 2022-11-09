39a145caa66a3a6e4dab295360603ca8faee5dd6
Lucx33 <lucaolima33@gmail.com>
2022-11-08 22:33:09 -0300

    aaa

--- SIte/teste.py
+++ SIte/teste.py
@@ -1,9 +1,46 @@
-from flask import Blueprint
+from flask import Flask, redirect, url_for, render_template, request, abort
+import pymongo
+from bson.objectid import ObjectId
 
 
 
-views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return render_template("login.html")
+app = Flask(__name__)
+
+
+
+
+# client connected to mongodb atlas
+client = pymongo.MongoClient("mongodb+srv://pstud:gVJQTsM2ftVKES5d@inf1039cardapuc.1cskrne.mongodb.net/?retryWrites=true&w=majority")
+# database named cardapuc
+db = client.cardapuc
+# collection named restaurant
+col = db.restaurantes
+
+# this creates a route at url / with methods post and get, associated with a form with input text value in the index.html file
+@app.route("/", methods=["POST", "GET"])
+def landing_page():
+    # by checking the method to be POST, it takes the input text value and parses the collection for the given key (in this case the name)
+    if request.method == "POST":
+        # takes the input value
+        restname = request.form["nome"]
+        # parses collection for given key
+        result = col.find_one({"nome": restname})
+        # checks whether or not the key is present in the collection
+        try:
+            # turns the id of the found object into a string (may not be strictly necessary)
+            result['_id'] = str(result['_id'])
+            # shows the information found
+            # return "<p>Horario: {}</p>".format(result["horario"])
+            # return redirect("/{}/".format(restname))
+        # aborts with error 404 if not found
+        except:
+            abort(404)
+        return render_template("index.html", result = result)
+    
+    return render_template("index.html")
+
+@app.route("/<name>/")
+def restaurant_page(name):
+    result = col.find_one({"nome": name})
+    return render_template("rest.html", result = result)
